0096422811ea029c8de102687a54ad557b9c3114
김계현 <ghkim@daou.co.kr>
2017-04-21 10:32:49 +0900

    0.00.084 register_image.bash 수정

--- bin/register_image.bash
+++ bin/register_image.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ### ================================================================================================
-###     프로그램 명     : register_image.bash, Version 0.00.006
+###     프로그램 명     : register_image.bash, Version 0.00.007
 ###     프로그램 설명   : Controller 장비에서 이미지를 등록 한다.
 ###     작성자          : 산사랑 (pnuskgh@gmail.com, www.jopenbusiness.com)
-###     작성일          : 2017.01.04 ~ 2017.04.06
+###     작성일          : 2017.01.04 ~ 2017.04.21
 ### ----[History 관리]------------------------------------------------------------------------------
 ###     수정자          :
 ###     수정일          :
@@ -44,64 +44,78 @@ else
 fi
 
 ### ------------------------------------------------------------------------------------------------
-###     Image를 OpenStack에 등록 한다.
+###     funcStartWith, 2016.1.9 ~ 2016.2.3, Version 0.00.003
+###     Using
+###         funcStartWith 문자열 비교할_문자열_목록
+###         funcStartWith ${NODE} controller
 ### ------------------------------------------------------------------------------------------------
-cd ~
+funcStartWith() {
+    local source=$1
+    local match=$2
+    local tmpstr
+
+    tmpstr=`expr match ${source} "\(^${match}\).*"`
+    if [[ "${match}" == "${tmpstr}" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+### ------------------------------------------------------------------------------------------------
+###     register_image : Image를 OpenStack에 등록 한다.
+### ------------------------------------------------------------------------------------------------
+register_image() {
+    local FILE_EXT=$1
+    local FILE_TYPE=$2
+
+    for IMAGE_FILE in `ls *.${FILE_EXT}`; do
+        IMAGE_NAME=`basename -s .${FILE_EXT} ${IMAGE_FILE}`
+
+        OPTIONS="--container-format bare --disk-format ${FILE_TYPE} --file ${IMAGE_FILE}"
+
+        funcStartWith ${IMAGE_NAME} Windows_Server_
+        if [[ "$?" == "1" ]]; then
+            OPTIONS="--min-disk 32 --min-ram 1024"
+        fi
+
+        funcStartWith ${IMAGE_NAME} MSSQL_Server_
+        if [[ "$?" == "1" ]]; then
+            OPTIONS="--min-disk 64 --min-ram 1024"
+        fi
+
+        funcStartWith ${IMAGE_NAME} CentOS_
+        if [[ "$?" == "1" ]]; then
+            OPTIONS="--min-disk 8 --min-ram 512"
+        fi
+
+        if [[ "zz${OPTIONS}zz" = "zzzz" ]]; then
+            OPTIONS="--min-disk 8 --min-ram 512"
+        fi
+        OPTIONS="--container-format bare --disk-format ${FILE_TYPE} ${OPTIONS} --file ${IMAGE_FILE}"
+
+        openstack image delete ${IMAGE_NAME}_${YEAR_MONTH} > /dev/null 2>&1
+        if [[ "z${FLAG_PUBLIC}z" = "zz" ]]; then
+            openstack image create ${OPTIONS} --private --project ${PROJECT_ID} ${IMAGE_NAME}_${YEAR_MONTH}
+        else
+            openstack image create ${OPTIONS} --public ${IMAGE_NAME}_${YEAR_MONTH}
+        fi
+        rm  ${IMAGE_FILE}
+
+        # openstack image set --property product_os='{ "code": "WIN_SVR_STD_2012_R2", "name": "Windows Server Standard 2012 R2", "charge_type": "month" }'
+        # openstack image set --property product_os='{ "code": "WIN_SVR_STD_2008_R2", 'name": "Windows Server Standard 2008 R2", "charge_type": "month" }'
+        # openstack image set --property product_dbms='{ "code": "MSSQL_SVR_2012", "name": "Microsoft SQL Server 2012 R2", "charge_type": "month" }'
+        # openstack image set --property product_dbms='{ "code": "MSSQL_SVR_2008", "name": "Microsoft SQL Server 2008 R2", "charge_type": "month" }'
+    done
+}
+
+### ------------------------------------------------------------------------------------------------
+###     Main process
+### ------------------------------------------------------------------------------------------------
 source ~/openrc
 
-for IMAGE_FILE in `ls *.qcow2`; do
-    IMAGE_NAME=`basename -s .qcow2 ${IMAGE_FILE}`
-
-    if [[ "zz${IMAGE_NAME}zz" = "zzCentOS_7_64zz" ]]; then
-        OPTIONS="--container-format bare --disk-format qcow2 --min-disk 8 --min-ram 512 --file ${IMAGE_FILE}"
-    elif [[ "zz${IMAGE_NAME}zz" = "zzCentOS_6_64zz" ]]; then
-        OPTIONS="--container-format bare --disk-format qcow2 --min-disk 8 --min-ram 512 --file ${IMAGE_FILE}"
-    elif [[ "zz${IMAGE_NAME}zz" = "zzMSSQL_Server_2012_R2zz" ]]; then
-        OPTIONS="--container-format bare --disk-format qcow2 --min-disk 64 --min-ram 1024 --file ${IMAGE_FILE}"
-    elif [[ "zz${IMAGE_NAME}zz" = "zzMSSQL_Server_2008_R2zz" ]]; then
-        OPTIONS="--container-format bare --disk-format qcow2 --min-disk 64 --min-ram 1024 --file ${IMAGE_FILE}"
-    else
-        OPTIONS="--container-format bare --disk-format qcow2 --min-disk 32 --min-ram 1024 --file ${IMAGE_FILE}"
-    fi
-
-    openstack image delete ${IMAGE_NAME}_${YEAR_MONTH}
-    if [[ "z${FLAG_PUBLIC}z" = "zz" ]]; then
-        openstack image create ${OPTIONS} --private --project ${PROJECT_ID} ${IMAGE_NAME}_${YEAR_MONTH}
-    else
-        openstack image create ${OPTIONS} --public ${IMAGE_NAME}_${YEAR_MONTH}
-        echo openstack image set --property product_os='{ "code": "WIN_SVR_STD_2012_R2", "name": "Windows Server Standard 2012 R2", "charge_type": "month" }'
-        echo openstack image set --property product_os='{ "code": "WIN_SVR_STD_2008_R2", 'name": "Windows Server Standard 2008 R2", "charge_type": "month" }'
-        echo openstack image set --property product_dbms='{ "code": "MSSQL_SVR_2012", "name": "Microsoft SQL Server 2012 R2", "charge_type": "month" }'
-        echo openstack image set --property product_dbms='{ "code": "MSSQL_SVR_2008", "name": "Microsoft SQL Server 2008 R2", "charge_type": "month" }'
-    fi
-
-    rm  ${IMAGE_FILE}
-done
-
-for IMAGE_FILE in `ls *.raw`; do
-    IMAGE_NAME=`basename -s .raw ${IMAGE_FILE}`
-
-    if [[ "zz${IMAGE_NAME}zz" = "zzCentOS_7_64zz" ]]; then
-        OPTIONS="--container-format bare --disk-format raw --min-disk 8 --min-ram 512 --file ${IMAGE_FILE}"
-    elif [[ "zz${IMAGE_NAME}zz" = "zzCentOS_6_64zz" ]]; then
-        OPTIONS="--container-format bare --disk-format raw --min-disk 8 --min-ram 512 --file ${IMAGE_FILE}"
-    elif [[ "zz${IMAGE_NAME}zz" = "zzMSSQL_Server_2012_R2zz" ]]; then
-        OPTIONS="--container-format bare --disk-format raw --min-disk 64 --min-ram 1024 --file ${IMAGE_FILE}"
-    elif [[ "zz${IMAGE_NAME}zz" = "zzMSSQL_Server_2008_R2zz" ]]; then
-        OPTIONS="--container-format bare --disk-format raw --min-disk 64 --min-ram 1024 --file ${IMAGE_FILE}"
-    else
-        OPTIONS="--container-format bare --disk-format raw --min-disk 32 --min-ram 1024 --file ${IMAGE_FILE}"
-    fi
-
-    openstack image delete ${IMAGE_NAME}_${YEAR_MONTH}
-    if [[ "z${FLAG_PUBLIC}z" = "zz" ]]; then
-        openstack image create ${OPTIONS} --private --project ${PROJECT_ID} ${IMAGE_NAME}_${YEAR_MONTH}
-    else
-        openstack image create ${OPTIONS} --public ${IMAGE_NAME}_${YEAR_MONTH}
-    fi
-
-    rm  ${IMAGE_FILE}
-done
+register_image raw   raw
+register_image qcow2 qcow2
 
 exit 0
 ### ================================================================================================
--- bin/scp_image.bash
+++ bin/scp_image.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ### ================================================================================================
-###     프로그램 명     : scp_image.bash, Version 0.00.002
+###     프로그램 명     : scp_image.bash, Version 0.00.003
 ###     프로그램 설명   : Image 파일을 복사 한다.
 ###     작성자          : 산사랑 (pnuskgh@gmail.com, www.jopenbusiness.com)
-###     작성일          : 2017.03.10 ~ 2017.04.06
+###     작성일          : 2017.03.10 ~ 2017.04.21
 ### ----[History 관리]------------------------------------------------------------------------------
 ###     수정자          :
 ###     수정일          :
@@ -18,12 +18,7 @@ TIMESTAMP=`date +%Y%m%d_%H%M%S`
 TARGET_SERVER=$1
 TARGET_FOLDER=$2
 
-for FILE_NAME in `ls *.qcow2`; do
-    scp ${FILE_NAME} ${TARGET_SERVER}:${TARGET_FOLDER}
-    mv ${FILE_NAME} ${FILE_NAME}_${TIMESTAMP}
-done
-
-for FILE_NAME in `ls *.raw`; do
+for FILE_NAME in `ls *.raw *.qcow2`; do
     scp ${FILE_NAME} ${TARGET_SERVER}:${TARGET_FOLDER}
     mv ${FILE_NAME} ${FILE_NAME}_${TIMESTAMP}
 done
