4adac758a48ac285ee1c4babff87914d63540677
Mountain Lover <consult@jopenbusiness.com>
2017-06-21 06:58:58 +0000

    0.00.102 updae vim.bash

--- bin/vim.bash
+++ bin/vim.bash
@@ -1,36 +1,34 @@
 #!/usr/bin/env bash
 ### ================================================================================================
-###     프로그램 명     : vim.bash, Version 0.00.001
-###     프로그램 설명   : Remote로 접속하여 명령 실행
+###     프로그램 명     : vim.bash, Version 0.00.002
+###     프로그램 설명   : Fuel에서 Remote로 접속하여 명령 실행
 ###     작성자          : 산사랑 (consult@jopenbusiness.com, www.jopenbusiness.com)
-###     작성일          : 2016.2.3 ~ 2016.2.3
+###     작성일          : 2016.2.3 ~ 2017.6.21
 ### --- [Copyright] --------------------------------------------------------------------------------
 ###     Copyright (c) 1995~2016 산사랑, All rights reserved.
 ### ================================================================================================
 
-if [[ "${SERVER_FOLDER}" == "" ]]; then
-    info "SERVER_FOLDER 환경 변수를 설정 하세요."
-    info " "
-    exit 1
+if [[ "z${HOME_SERVICE}z" == "zz" ]]; then
+    export HOME_SERVICE="/service"
 fi
 
-. ${SERVER_FOLDER}/bin/config.bash > /dev/null 2>&1
-. ${SERVER_FOLDER}/bin/utilCommon.bash > /dev/null 2>&1
+. ${HOME_SERVICE}/bin/config.bash > /dev/null 2>&1
+. ${HOME_SERVICE}/bin/utilCommon.bash > /dev/null 2>&1
 
 ### ------------------------------------------------------------------------------------------------
 ###     funcUsing, 2016.2.3 ~ 2016.2.3, Version 0.00.001
 ###     사용법을 표시 합니다.
 ### ------------------------------------------------------------------------------------------------
 funcUsing() {
-    info "Using : vim.bash NODES FILES"
-    info "        NODES          : 실행할 nodes"
-    info "        FILES          : 수정할 파일"
-    info " "
+    echo "Using : vim.bash NODES FILES"
+    echo "        NODES          : 실행할 nodes"
+    echo "        FILES          : 수정할 파일"
+    echo " "
     exit 2
 }
 
 ### ------------------------------------------------------------------------------------------------
-###     Main
+###     Main Process
 ### ------------------------------------------------------------------------------------------------
 ###---  Command Line에서 입력된 인수를 검사한다.
 if [[ 1 < $# ]]; then
