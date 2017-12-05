b45ea492844965e2b58dc966b159df2739460bd9
Mountain Lover <consult@jopenbusiness.com>
2017-12-05 16:28:41 +0900

    update liferay

---
+++ service_sw/Liferay_Portal/bin/deployManual.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+### ================================================================================================
+###     프로그램 명     : deployManual.bash, Version 0.00.001
+###     프로그램 설명   : Liferay Portal을 설치 한다.
+###     작성자          : 산사랑 (pnuskgh@gmail.com, www.jopenbusiness.com)
+###     작성일          : 2017.12.04 ~ 2017.12.04
+### ----[History 관리]------------------------------------------------------------------------------
+###     수정자          :
+###     수정일          :
+###     수정 내용       :
+### --- [Copyright] --------------------------------------------------------------------------------
+###     Copyright (c) 1995~2017 pnuskgh, 오픈소스 비즈니스 컨설팅
+###     All rights reserved.
+### ================================================================================================
+
+### ------------------------------------------------------------------------------------------------
+###     실행 환경을 설정 한다.
+### ------------------------------------------------------------------------------------------------
+source ${HOME_SERVICE}/bin/config.bash > /dev/null 2>&1
+source ${UTIL_DIR}/common.bash > /dev/null 2>&1
+
+RELATION_DIR="$(dirname $0)"
+WORKING_DIR="$(cd -P ${RELATION_DIR}/.. && pwd)"
+# WORKING_DIR="/service/service_sw/Liferay_Portal"
+source ${WORKING_DIR}/bin/config.bash
+
+### ------------------------------------------------------------------------------------------------
+###     Liferay Portal 7.0.4 GA5 with Apache Tomcat 매뉴얼 배포
+###     http://demo.obcon.co.kr/doc_liferay/
+### ------------------------------------------------------------------------------------------------
+cd ${DOCUMENT_ROOT}
+unzip ${HOME_WORK}/install/liferay-ce-portal-doc-7.0-ga5-20171018150113838.zip
+mv liferay-ce-portal-doc-7.0-ga5 doc_liferay
+chown -R nginx:nginx doc_liferay
+cp ${TEMPLATE_DIR}/index.html doc_liferay//index.html
+chown nginx:nginx doc_liferay//index.html
+
+### ================================================================================================
+
--- service_sw/Liferay_Portal/bin/install.bash
+++ service_sw/Liferay_Portal/bin/install.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ### ================================================================================================
-###     프로그램 명     : 001_install.bash, Version 0.00.001
+###     프로그램 명     : install.bash, Version 0.00.002
 ###     프로그램 설명   : Liferay Portal을 설치 한다.
 ###     작성자          : 산사랑 (pnuskgh@gmail.com, www.jopenbusiness.com)
-###     작성일          : 2017.12.04 ~ 2017.12.04
+###     작성일          : 2017.12.04 ~ 2017.12.05
 ### ----[History 관리]------------------------------------------------------------------------------
 ###     수정자          :
 ###     수정일          :
@@ -25,15 +25,31 @@ WORKING_DIR="$(cd -P ${RELATION_DIR}/.. && pwd)"
 source ${WORKING_DIR}/bin/config.bash
 
 ### ------------------------------------------------------------------------------------------------
-###     Liferay Portal 7.0.4 GA5 with Apache Tomcat 매뉴얼 배포
-###     http://demo.obcon.co.kr/doc_liferay/
+###     funcUsing()
+###         사용법 표시
 ### ------------------------------------------------------------------------------------------------
-cd ${DOCUMENT_ROOT}
-unzip ${HOME_WORK}/install/liferay-ce-portal-doc-7.0-ga5-20171018150113838.zip
-mv liferay-ce-portal-doc-7.0-ga5 doc_liferay
-chown -R nginx:nginx doc_liferay
-cp ${TEMPLATE_DIR}/index.html doc_liferay//index.html
-chown nginx:nginx doc_liferay//index.html
+funcUsing() {
+    /bin/echo "Using : install.bash DATABASE USER PASSWORD [ROOTPASSWORD]"
+    /bin/echo "        DATABASE       : Database"
+    /bin/echo "        USER           : Database 사용자"
+    /bin/echo "        PASSWORD       : Database 사용자의 암호"
+    /bin/echo "        ROOTPSSWORD    : Database root 사용자의 암호"
+    /bin/echo " "
+    exit 1
+}
+
+###---  Command Line에서 입력된 인수를 검사한다.
+if [[ $# = 3 ]]; then
+    /bin/echo -n "Database root 사용자의 암호를 입력 하세요 : "
+    read ROOTPASSWORD
+elif [[ $# = 4 ]]; then
+    ROOTPASSWORD=$4
+else
+    funcUsing
+fi
+DATABASE=$1
+USER=$2
+PASSWORD=$3
 
 ### ------------------------------------------------------------------------------------------------
 ###     Liferay Portal 7.0.4 GA5 with Apache Tomcat 설치
@@ -49,6 +65,24 @@ firewall-cmd --permanent --zone=public --add-port=8080/tcp
 firewall-cmd --reload
 firewall-cmd --list-all
 
+### ------------------------------------------------------------------------------------------------
+###     MariaDB 구성
+### ------------------------------------------------------------------------------------------------
+${HOME_SERVICE}/service_sw/MariaDB/bin/createDatabase.bash ${DATABASE} ${USER} ${PASSWORD} ${ROOTPASSWORD}
+
+TMPFILE=${DOCUMENT_ROOT}/liferay/portal-ext.properties
+echo "jdbc.default.driverClassName=org.mariadb.jdbc.Driver" >> ${TMPFILE}
+echo "jdbc.default.url=jdbc:mariadb://localhost/${DATABASE}?useUnicode=true&characterEncoding=UTF-8&useFastDateParsing=false" >> ${TMPFILE}
+echo "jdbc.default.username=${USER}" >> ${TMPFILE}
+echo "jdbc.default.password=${PASSWORD}" >> ${TMPFILE}
+
+cat ${TMPFILE}
+echo " "
+
+### ------------------------------------------------------------------------------------------------
+###     Liferay Portal 기동/종료
+### ------------------------------------------------------------------------------------------------
+cd ${DOCUMENT_ROOT}
 cd liferay/tomcat-8.0.32/bin
 ./startup.sh
 
